f00119b929d1b4cf82c0d161381f13b0693cf854
rurtubiac <randy.urtubia@inacapmail.cl>
2015-11-29 23:17:01 -0300

    getting the finished project

--- AutomotaNotNull_/Buscar_Vehiculo_Cliente.aspx.designer.cs
+++ AutomotaNotNull_/Buscar_Vehiculo_Cliente.aspx.designer.cs
@@ -10,7 +10,7 @@
 namespace AutomotaNotNull_ {
     
     
-    public partial class Ingresar_Vehiculo_Admin {
+    public partial class Buscar_Vehiculo_Cliente {
         
         /// <summary>
         /// form1 control.
@@ -22,21 +22,21 @@ namespace AutomotaNotNull_ {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
-        /// DetailsView1 control.
+        /// GridView1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.DetailsView DetailsView1;
+        protected global::System.Web.UI.WebControls.GridView GridView1;
         
         /// <summary>
-        /// DSVehiculo control.
+        /// DSAutos control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ObjectDataSource DSVehiculo;
+        protected global::System.Web.UI.WebControls.ObjectDataSource DSAutos;
     }
 }
